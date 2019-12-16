3b7c356b14d70b40d7f2e3bb769802c82e940acf
Anjali Konda <anjalirajani@gmail.com>
2019-12-16 10:45:14 -0600

    added another project to the Repository

---
+++ MyCode/src/com/gm/sample/Calculator.java
@@ -0,0 +1,15 @@
+package com.gm.sample;
+
+public class Calculator {
+	public int add(int a,int b) {
+		int r = a + b;
+		return r;
+	}
+	public int sub(int a, int b) {
+		int r = a - b;
+		return r;
+		
+	}
+
+
+}
